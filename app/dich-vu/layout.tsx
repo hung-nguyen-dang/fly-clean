6c8937e2f46ecac1420b76f50946359d02424ce5
hung_nguyendang <hung.nguyendang@sinch.com>
2025-04-19 12:14:10 +0700

    bugfix: update lint

--- app/dich-vu/layout.tsx
+++ app/dich-vu/layout.tsx
@@ -3,7 +3,9 @@ import { ReactNode } from 'react'
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex w-full flex-1 justify-center">
-      <div className="container mt-4 px-8 md:mt-8 md:px-12 lg:px-16">{children}</div>
+      <div className="container mt-4 px-8 md:mt-8 md:px-12 lg:px-16">
+        {children}
+      </div>
     </div>
   )
 }
